7ba8caf431bdd5000536b3ee19d415f5d99b9194
Vinn Nduri <ndurivo@gmail.com>
2022-06-13 10:33:02 +0300

    commit tree 359206e8f075c22ec77c0a69e5bc439b55de1330

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
 
-let totalBatteries = 0;
+let totalBatteries;
+
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+}
 
 totalBatteries = 31;
